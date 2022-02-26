bbb18b20eae58f6e6281d43c0dee3d82fb43171b
Anya <82313956+anya768@users.noreply.github.com>
2022-02-26 19:13:37 +0200

    Add exercises 3.9-3.11

---
+++ Part3/phonebook_backend/README.md
@@ -0,0 +1,13 @@
+# Phonebook App
+
+## How to Start the App
+
+From your console run the following command:
+
+`
+    npm run dev
+`
+
+
+## Live Version
+https://rocky-meadow-63363.herokuapp.com/
--- Part3/phonebook_backend/index.js
+++ Part3/phonebook_backend/index.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body', (req,res) => JSON.stringify(req.body))
 app.use(express.json())
